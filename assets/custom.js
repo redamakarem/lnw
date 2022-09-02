d7cd9e2fbf847e44a93e2c08bfd7ef3d9fc94b7a
Reda Makarem <makarem.reda@gmail.com>
2022-09-02 20:12:41 +0300

    ..

--- assets/custom.js
+++ assets/custom.js
@@ -20,3 +20,39 @@
   // ^^ Keep your scripts inside this IIFE function call to 
   // avoid leaking your variables into the global scope.
 })();
+
+
+var bannerHeight = 0;
+        var scrollVal = 0
+        var footerTop = 0
+        var perCol = 0
+        var myOffset = 0
+        var colHeight = 0;
+        var initialMarginTop = 0
+        var currentMarginTop = 0;
+        var footerTop =0
+        $(document).ready(function(){
+             footerTop = $('footer').offset().top - $('footer').height();
+            bannerHeight = $('.banner').height();
+            colHeight = $('.col-2').height();
+            initialMarginTop = ($('.col-2').height() - $('.col-2 .col-box').first().height())*-1 ;
+            perCol = $('.col-2').children.length;
+            myOffset = perCol -1;
+            $('.col-2').css('margin-top',initialMarginTop + 'px')
+
+
+        });
+        $(window).scroll(function(){
+            if($(window).scrollTop()>bannerHeight && $(window).scrollTop()<footerTop){
+                scrollVal = $(window).scrollTop() - bannerHeight;
+                $(":root").css('--scroll-val',scrollVal+'px')
+            }
+            else if($(window).scrollTop()<bannerHeight){
+                scrollVal = 0;
+                $(":root").css('--scroll-val',scrollVal+'px');
+            }
+            else if($(window).scrollTop()>footerTop){
+                scrollVal = initialMarginTop;
+                $(":root").css('--scroll-val',scrollVal+'px');
+            }
+        });
